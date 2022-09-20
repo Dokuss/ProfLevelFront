700adc183f27edf9d82e080fffb31df23deaa474
Dokuss <dokus5830@gmail.com>
2022-09-20 22:07:58 +0200

    ADD Lesson1

---
+++ Lesson1/script.js
@@ -0,0 +1,76 @@
+//console.log(100) вывод в консоль//
+// let a = 10
+// let b = 'Hello World'
+
+// a = 'Hello'
+
+//const a = 'Hello World'
+
+
+// let изменяемая переменная
+
+// const неизменяемая переменная
+
+// var изменяемая, старое, до ES6 
+
+// console.log(a)
+// console.log(b)
+
+// let a = 'Hello'
+// let b = 'World!'
+
+// console.log(a+' '+b) конкатинация
+
+// console.log(1123 + 'string') неявное преобразование
+
+// let a = 'Pri\nvet' \n - перенос
+// let b = "Privet"
+
+// let c = `Pr
+// iv
+// et` апостроф - позволяют переносить текст на новую строку
+
+// console.log(a)
+// console.log(c)
+
+// let a = 'Privet'
+
+// let b = "Moj"
+
+// let c = 'Drug'
+
+// console.log(a+', '+b+' '+c+'!')
+
+// console.log(`${a}, ${b} ${c}!`)  интерполяция - аналог вывода выше
+
+// let a = 5.4
+// console.log(a)
+
+// let b = 5 / 'string'
+// console.log(typeof b) функция позволяющая определить тип данных
+// console.log(typeof (a))
+
+// Global object Math
+
+// let a = 5.4
+// let b = 5
+
+// console.log(Math.round(a))  - округление по правилам матиматики
+
+// console.log(Math.ceil(a))  - округляет вверх
+// console.log(Math.floor(a))  - округление вниз
+
+// console.log(Math.pow(b,5)) - возведение в степень (аналог b ** 5)
+// console.log(b ** 5)
+// console.log(Math.sqrt(625)) - квадратный корень
+
+// console.log(Math.sqrt(Math.sqrt(625))) - кубический корень
+
+// console.log(Math.abs(15)) - отбрасывает знак числа
+
+// console.log(Math.sign(9)) - возвращает знак числа (1, -1, 0)
+
+// console.log(Math.random() * 100) - выводит рандомное число
+
+// let result = Math.random() * 1000
+// console.log(Math.floor(result))
